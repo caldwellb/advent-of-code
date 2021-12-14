fa316af9c61a7e6e928c2359337853e10b5c5c2b
Ben Caldwell <caldwellb@uchicago.edu>
2021-12-14 01:01:21 -0600

    Run #2 at Day 14

--- 2021/14/Day14.hs
+++ 2021/14/Day14.hs
@@ -1,17 +1,12 @@
 {-# LANGUAGE ExtendedDefaultRules #-}
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import Data.List.Split
-import Data.Array as SA
-import Data.Array.IO
-import Data.IORef
-import Control.Monad
-import qualified Data.Text as T
-default (T.Text)
 
 type PolymerChain     = String
 type Polymer          = Char
 type InsertionRules   = M.Map (Polymer, Polymer) Polymer
-type OccurenceCounter = IOArray Polymer Int
+type Occurences       = M.Map Polymer Int
+type Depth            = Int
 
 parseLine :: String -> ((Polymer,Polymer), Polymer)
 parseLine = (,) <$> ((,) <$> head <*> (!! 1)) <*> (!! 6)
@@ -19,47 +14,31 @@ parseLine = (,) <$> ((,) <$> head <*> (!! 1)) <*> (!! 6)
 parseInput :: String -> (PolymerChain, InsertionRules)
 parseInput = ((,) <$> head <*> M.fromList . map parseLine . drop 2) . lines
 
-chainStep :: OccurenceCounter -> InsertionRules -> PolymerChain -> IO PolymerChain
-chainStep ctr pairInsert (x:y:zs) = do
-    let newPolymer = pairInsert M.! (x, y)
-    increaseOccurenceCounter ctr newPolymer
-    others <- chainStep ctr pairInsert (y:zs)
-    return (x : newPolymer : others)
-chainStep ctr pairInsert chain = return chain
-
 possiblePolymers :: [Polymer]
 possiblePolymers = ['B', 'C', 'F', 'H', 'K', 'N', 'O', 'P', 'S', 'V']
 
-countOf :: Polymer -> PolymerChain -> Int
-countOf x = length . filter (== x)
+emptyOccurences :: Occurences
+emptyOccurences = M.fromList (zip possiblePolymers [0, 0..])
 
-highestCountPolymer :: PolymerChain -> Int
-highestCountPolymer chain = maximum $ (`countOf` chain) <$> possiblePolymers
+spotPolymer :: Polymer -> Occurences -> Occurences
+spotPolymer = flip (M.insertWith (+)) 1
 
-lowestCountPolymer :: PolymerChain -> Int
-lowestCountPolymer chain = minimum . filter (/= 0) $ (`countOf` chain) <$> possiblePolymers 
+chainStep :: Depth -> InsertionRules -> (Polymer, Polymer) -> Occurences
+chainStep 0  rules (x, y) = emptyOccurences
+chainStep sn rules (x, y) = spotPolymer (rules M.! (x,y)) (M.unionWith (+) (chainStep (sn - 1) rules (x, rules M.! (x,y))) (chainStep (sn - 1) rules (rules M.! (x,y), y)))
 
-increaseOccurenceCounter :: OccurenceCounter -> Polymer -> IO ()
-increaseOccurenceCounter counter polymer = readArray counter polymer >>= writeArray counter polymer . (1+)
-
-makeOccurenceCounter :: PolymerChain -> IO OccurenceCounter
-makeOccurenceCounter chain = do
-    blankArray <- newArray ('B', 'V') 0 
-    mapM_ (blankArray `increaseOccurenceCounter`) chain
-    return blankArray
+spotTemplate :: Occurences -> PolymerChain -> Occurences
+spotTemplate = foldr spotPolymer
 
 main :: IO ()
 main = do
-    (template, rules) <- parseInput <$> readFile "input"
-    ctr <- makeOccurenceCounter template
-    ref <- newIORef template
-    forM_ ([1..10] :: [Int]) $ \_ -> do
-        readIORef ref >>= chainStep ctr rules >>= writeIORef ref
-    maxVal <- maximum <$> (freeze ctr :: IO (Array Polymer Int))
-    minVal <- foldl1 (\x y -> if y == 0 then x else min x y) <$> (freeze ctr :: IO (Array Polymer Int))
-    putStr "Solution 1: " >> print (maxVal - minVal)
-    forM_ ([11..40] :: [Int]) $ \_ -> do
-        readIORef ref >>= chainStep ctr rules >>= writeIORef ref
-    maxVal <- maximum <$> (freeze ctr :: IO (Array Polymer Int))
-    minVal <- foldl1 (\x y -> if y == 0 then x else min x y) <$> (freeze ctr :: IO (Array Polymer Int))
+    (template, rules) <- parseInput <$> readFile "testinput"
+    let first10 = (`spotTemplate` template) . M.unionsWith (+) $ zipWith (curry (chainStep 10 rules)) template (tail template)
+    let max10 = maximum first10
+    let min10 = minimum first10
+    putStr "Solution 1: " >> print (max10 - min10)
+    let then40 = (`spotTemplate` template) . M.unionsWith (+) $ zipWith (curry (chainStep 40 rules)) template (tail template)
+    let max40 = maximum then40
+    let min40 = minimum then40
+    putStr "Solution 2: " >> print (max40 - min40)
     return ()

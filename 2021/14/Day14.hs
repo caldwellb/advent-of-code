ffc2c4657bfdc7b22b7a3757d73372aeb4c43a4b
Ben Caldwell <caldwellb@uchicago.edu>
2021-12-14 22:30:57 -0600

    Merge? No idea why.

--- 2021/14/Day14.hs
+++ 2021/14/Day14.hs
@@ -27,17 +27,17 @@ chainStep :: Depth -> InsertionRules -> (Polymer, Polymer) -> Occurences
 chainStep 0  rules (x, y) = emptyOccurences
 chainStep sn rules (x, y) = spotPolymer (rules M.! (x,y)) (M.unionWith (+) (chainStep (sn - 1) rules (x, rules M.! (x,y))) (chainStep (sn - 1) rules (rules M.! (x,y), y)))
 
-spotTemplate :: Occurences -> PolymerChain -> Occurences
-spotTemplate = foldr spotPolymer
+scoreTemplate :: Occurences -> PolymerChain -> Occurences
+scoreTemplate = foldr spotPolymer
 
 main :: IO ()
 main = do
-    (template, rules) <- parseInput <$> readFile "testinput"
-    let first10 = (`spotTemplate` template) . M.unionsWith (+) $ zipWith (curry (chainStep 10 rules)) template (tail template)
+    (template, rules) <- parseInput <$> readFile "input"
+    let first10 = (`scoreTemplate` template) . M.unionsWith (+) $ zipWith (curry (chainStep 10 rules)) template (tail template)
     let max10 = maximum first10
     let min10 = minimum first10
     putStr "Solution 1: " >> print (max10 - min10)
-    let then40 = (`spotTemplate` template) . M.unionsWith (+) $ zipWith (curry (chainStep 40 rules)) template (tail template)
+    let then40 = (`scoreTemplate` template) . M.unionsWith (+) $ zipWith (curry (chainStep 40 rules)) template (tail template)
     let max40 = maximum then40
     let min40 = minimum then40
     putStr "Solution 2: " >> print (max40 - min40)
